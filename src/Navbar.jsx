a0eadac66ccaf4498cf3ec93ce11ed26c65cb929
Xhoel <xhoel.seitllari@fshnstudent.info>
2025-04-23 14:47:10 +0200

    last touch

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,8 +5,8 @@ function Navbar({ filter, setFilter }) {
 
   return (
     <>
-      <div className="sticky top-0">
-        <div className="bg-[#161b22]  text-amber-50 flex items-center gap-1.5 p-3 text-xl rounded-2xl m-3 max-[600px]:flex max-[600px]:justify-center max-[600px]:py-2.5 max-[600px]:text-2xl ">
+      <div className="sticky top-0 bg-[#0b0d10] z-1 py-4">
+        <div className="bg-[#161b22]  text-amber-50 flex items-center gap-1.5 p-3 text-xl rounded-2xl m-8 max-[600px]:flex max-[600px]:justify-center max-[600px]:py-2.5 max-[600px]:text-2xl ">
           <h1>Extensions</h1>
         </div>
         <div className="flex justify-between px-10 max-[600px]:flex max-[600px]:justify-center max-[600px]:items-center max-[600px]:gap-4 max-[600px]:flex-col">
